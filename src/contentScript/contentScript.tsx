dff8605821b4f1237e44c098a403f4c99e6a22b6
Burak <burakduruk52@gmail.com>
2023-06-15 20:55:50 -0400

    new newnew

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import { AppBar, FormControlLabel, ListItem, Menu, ListItemButton, MenuItem, Typography, TextField, Divider, Box, Toolbar, Checkbox, IconButton, Modal } from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
-import { LoadingButton } from '@mui/lab'
-import { getVODInfo, getChatLogs, returnSearchedArray, checkChannel } from '../utils/api';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { getVODInfo, getChatLogs, returnSearchedArray, checkChannel, getChatLogsFromTwitch } from '../utils/api';
 import { Virtuoso } from 'react-virtuoso';
-import $ from 'jquery';
+import Switch from "react-switch";
 import './contentScript.css';
+import $ from 'jquery';
 import Draggable from 'react-draggable';
 
 let logArr = [];
@@ -14,15 +12,16 @@ let vodId: string = '';
 
 const Icon = () => {
     const [isInValid, setIsInValid] = useState<boolean>(true);
-    const [anchorEl, setAnchorEl] = useState<HTMLElement>(null);
     const [runText, setRunText] = useState<boolean>(true);
     const [loading, setLoading] = useState<boolean>(false);
     const [isChecked1, setIsChecked1] = useState<boolean>(false);
     const [isChecked2, setIsChecked2] = useState<boolean>(false);
+    const [isChecked3, setIsChecked3] = useState<boolean>(false);
     const [textVal, setTextVal] = useState<string>('');
     const [textLabel, setTextLabel] = useState<string>('');
     const [resultArray, setResultArray] = useState([]);
-    const [open, setOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
     const initate = async () => {
         let ret = getTwitchTokens();
@@ -45,13 +44,13 @@ const Icon = () => {
                 const ch = await checkChannel();
 
                 if(ch.channels.some(e => e.name === data.channelname)) {
-                    const logs = await getChatLogs(data.created_at, data.length, data.channelname);
-                    logArr = logs;
+                    logArr = await getChatLogs(data.created_at, data.length, data.channelname, setTextLabel);
                     setTextLabel('Input');
                     setLoading(false);
                     setRunText(false);
                 }
                 else {
+                    await getChatLogsFromTwitch(data.created_at, data.length, data.channelname, setTextLabel);
                     setLoading(false);
                     setTextLabel('Channel is not supported');
                 }
@@ -63,106 +62,116 @@ const Icon = () => {
     }
 
     const handleClick = () => {
-        if(textVal.length >= 3) {
-           setLoading(true);
-
-            if(isChecked2) {
-                setResultArray(returnSearchedArray(logArr, textVal, isChecked1, true));
-            }
-            else {
-                setResultArray(returnSearchedArray(logArr, textVal, isChecked1, false));
-            }
-            
-            setLoading(false); 
+        if(textVal.length >= 2) {
+            setResultArray(returnSearchedArray(logArr, textVal, isChecked1, isChecked2, isChecked3));
         }
-    }
-  
-    const handleClose = () => {
-      setAnchorEl(null)
-    }
-  
-    const handleMenuClick = (event) => {
-      setAnchorEl(event.currentTarget)
     }
 
     return (
         <div>
             <div className='searchsen-button'>
                 <button onClick={() => {
-                    //initate();
-                    setOpen(true)}}>Searchsen
+                    if (!isOpen) initate();
+                    setIsOpen(!isOpen)}}>
+                        Searchsen
                 </button>
             </div>
-            <Draggable>
-                <Modal open={open} onClose={() => setOpen(false)}>
-                    <div></div>
-                </Modal>
-            </Draggable>
-            
-
-
-
-            {/* <Modal open={open} onClose={() => setOpen(false)}>
-              <div>
-                  <Box sx={style}>
-                    <Box sx={{flexGrow: 1}}>
-                      <AppBar position='static' color='inherit'>
-                          <Toolbar>
-                              <IconButton onClick={handleMenuClick} size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-                                <MenuIcon />
-                              </IconButton>
-                              <Menu id='basic-menu' anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-                                <MenuItem > 
-                                  <FormControlLabel control={<Checkbox color='secondary' checked={isChecked1} onChange={()=>{setIsChecked1(!isChecked1)}}/>} label='Case sensitive' />
-                                </MenuItem>
-                                <MenuItem >
-                                  <FormControlLabel control={<Checkbox color='secondary' checked={isChecked2} onChange={()=>{setIsChecked2(!isChecked2)}}/>} label='Search by username' />
-                                </MenuItem>
-                                <Divider />
-                                <MenuItem onClick={()=>{window.open('https://github.com/burakdrk/searchsen')}}>
-                                  Source code and guide
-                                </MenuItem>
-                              </Menu>
-                                <Typography variant ='h6'
-                                  sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-                                  noWrap
-                                  component='div'>Searchsen</Typography>
-                              <Box sx={{width: 500, maxWidth: '50%'}}>
-                                <TextField fullWidth label={textLabel} id='fullWidth' size='small' color='secondary' disabled={runText} value={textVal} onChange={e=> {
-                                  if(e.target.value.length < 3){
-                                      setIsInValid(true)
-                                  }
-                                  else{
-                                    setIsInValid(false)
-                                  }
-                                  return setTextVal(e.target.value)}}/>
-                              </Box>
-                              <Divider sx={{ height: 28, m: 0.5 }} orientation='vertical' />
-                              <LoadingButton loading={loading} variant='contained' color='secondary' disabled={isInValid} onClick={async()=>{handleClick()}}>Search</LoadingButton>
-                          </Toolbar>
-                      </AppBar>
-                    </Box>
-                    <Box
-                      sx={{ width: '100%', height: '100%', maxWidth: '38em', bgcolor: 'background.paper' }}>     
-                        <Virtuoso 
-                          style={{height:'27em', width:'38em'}}
-                          totalCount={resultArray.length}
-                          itemContent={
-                            index => 
-                            <ListItem key={index} component='div' disablePadding>
-                              <ListItemButton component='a' onClick={()=>{
-                                const temp = document.getElementsByTagName('video')
-                                temp[0].currentTime = resultArray[index].inSecs
-                              }}>
-                                <Typography color='secondary'>{`[${resultArray[index].intoVod}] ${resultArray[index].user}: ${resultArray[index].message}`}</Typography>
-                              </ListItemButton>
-                            </ListItem>}/>
-                    </Box>
-                  </Box>
-              </div>
-            </Modal> */}
+            {isOpen && 
+            <Draggable handle='.searchsen-logo-top'>
+                <div className='searchsen-container'>
+                    <div className='searchsen-logo'>
+                        <div className='searchsen-logo-top'>
+                            <span className='searchsen-menu-logo' onClick={() => setIsMenuOpen(!isMenuOpen)}>⚙</span>
+                            Searchsen
+                            <span onClick={() => setIsOpen(false)}>ⓧ</span>
+                        </div>
+                        <div className='searchsen-menu' style={{display: isMenuOpen ? 'flex' : 'none', maxHeight: isMenuOpen ? '500px' : '0px'}}>
+                            <label>
+                                <Switch onColor="#9047ff"
+                                    onHandleColor="#6e36c2"
+                                    handleDiameter={18}
+                                    uncheckedIcon={false}
+                                    checkedIcon={false}
+                                    boxShadow="0px 1px 2.5px rgba(0, 0, 0, 0.6)"
+                                    activeBoxShadow="0px 0px 1px 5px rgba(0, 0, 0, 0.2)"
+                                    height={15}
+                                    width={32} checked={isChecked1} onChange={() => (setIsChecked1(!isChecked1), setIsChecked3(false))}/>
+                                <span>Case sensitive</span>
+                            </label>
+                            <label>
+                                <Switch onColor="#9047ff"
+                                    onHandleColor="#6e36c2"
+                                    handleDiameter={18}
+                                    uncheckedIcon={false}
+                                    checkedIcon={false}
+                                    boxShadow="0px 1px 2.5px rgba(0, 0, 0, 0.6)"
+                                    activeBoxShadow="0px 0px 1px 5px rgba(0, 0, 0, 0.2)"
+                                    height={15}
+                                    width={32} checked={isChecked2} onChange={() => (setIsChecked2(!isChecked2), setIsChecked3(false))}/>
+                                <span>Search by username</span>
+                            </label>
+                            <label>
+                                <Switch onColor="#9047ff"
+                                    onHandleColor="#6e36c2"
+                                    handleDiameter={18}
+                                    uncheckedIcon={false}
+                                    checkedIcon={false}
+                                    boxShadow="0px 1px 2.5px rgba(0, 0, 0, 0.6)"
+                                    activeBoxShadow="0px 0px 1px 5px rgba(0, 0, 0, 0.2)"
+                                    height={15}
+                                    width={32} checked={isChecked3} onChange={() => (setIsChecked3(!isChecked3), setIsChecked1(false), setIsChecked2(false))}/>
+                                <span>Regex search</span>
+                            </label>
+                            <div onClick={() => window.open('https://github.com/burakdrk/searchsen', '_blank')}>Guide and source code</div>
+                        </div>
+                    </div>
+                    <div className='searchsen-search' style={{padding: isMenuOpen ? '10px' : '4px 10px 10px'}}>
+                        <input disabled={runText} type='text' value={textVal} placeholder='Input' onChange={e => {
+                            setTextVal(e.target.value);
+                            e.target.value.length < 2 ? setIsInValid(true) : setIsInValid(false);
+                            }}></input>
+                        <button onClick={() => handleClick()} disabled={isInValid}>SEARCH</button>
+                    </div>
+                    <div className='searchsen-results' style={{display: runText ? 'none' : 'block'}}>
+                        <Virtuoso
+                            totalCount={resultArray.length}
+                            style={{height: isMenuOpen ? '229.5px' : '363.5px'}}
+                            itemContent = {(index) => {
+                                return (
+                                    <div className='searchsen-result-box' onClick={() => {
+                                        const temp = document.getElementsByTagName('video')
+                                        temp[0].currentTime = resultArray[index].inSecs}}>
+                                        <span style={{fontSize: '10px', color:'#ffffffbf'}}>{`[${resultArray[index].intoVod}] `}</span><span style={{fontWeight: '600', color: resultArray[index].userColor}}>{`${resultArray[index].user}:`}</span>{` ${resultArray[index].message}`}
+                                    </div>
+                                )}
+                            }
+                        />
+                    </div>
+                    <div className='searchsen-loading'>
+                        <div className='searchsen-loader' style={{display: loading ? 'block' : 'none'}}>
+                            <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
+                                <g fill="none" fillRule="evenodd">
+                                    <g transform="translate(1 1)" strokeWidth="2">
+                                        <circle strokeOpacity=".5" cx="18" cy="18" r="18"/>
+                                        <path d="M36 18c0-9.94-8.06-18-18-18">
+                                            <animateTransform
+                                                attributeName="transform"
+                                                type="rotate"
+                                                from="0 18 18"
+                                                to="360 18 18"
+                                                dur="1s"
+                                                repeatCount="indefinite"/>
+                                        </path>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <div className='searchsen-loading-text' style={{display: runText ? 'block' : 'none'}}>{textLabel}</div>
+                    </div>
+                </div>
+            </Draggable>}
         </div>
-    )
+    );
 }
 
 const getTwitchTokens = () => {
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -19,6 +19,10 @@ export const getVODInfo = async ({video_id, oauth, client_id}): Promise<any> => 
     return {channelname: data.data.video.creator.login, created_at: data.data.video.createdAt, length: data.data.video.lengthSeconds};
 }
 
+export const getChatLogsFromTwitch = async (creation: string, duration: number, username: string, setTextLabel: Function): Promise<any> => {
+    console.log('oh!');
+}
+
 export const checkChannel = async (): Promise<any> => {
     const res = await fetch('https://logs.ivr.fi/channels', {
         'method': 'GET',
@@ -35,41 +39,89 @@ export const checkChannel = async (): Promise<any> => {
     return data;
 }
 
-export const getChatLogs = async (creation: string, duration: number, username: string): Promise<any> => {
+export const getChatLogs = async (creation: string, duration: number, username: string, setTextLabel: Function): Promise<any> => {
     const date = new Date(creation);
     const endDate = moment(date).add(duration, 's').toDate();
- 
+
     const endEpoch = endDate.getTime();
     const startEpoch = date.getTime();
+    setTextLabel(`Downloading ${date.getUTCDate()}/${date.getUTCMonth()+1}/${date.getUTCFullYear()} logs...`);
   
     const res = await fetch(`https://logs.ivr.fi/channel/${username}/${date.getUTCFullYear()}/${date.getUTCMonth()+1}/${date.getUTCDate()}`, {
         'method': 'GET',
     });
 
     if(!res.ok) {
+        setTextLabel(`Failed retreiving logs, reload the tab and try again.`);
         throw new Error('Error retrieving chat');
-    };
+    }
 
     let data = await res.text();
-
     let lines = eol.split(data);
     lines.pop();
 
-    let newDate = moment(date).add(1, 'd').toDate();
+    let newDate = moment(date).add(1, 'd').utc().startOf('d').toDate();
     while(newDate.getTime() < endDate.getTime()) {
+        setTextLabel(`Downloading ${newDate.getUTCDate()}/${newDate.getUTCMonth()+1}/${newDate.getUTCFullYear()} logs...`);
+
         const res2 = await fetch(`https://logs.ivr.fi/channel/${username}/${newDate.getUTCFullYear()}/${newDate.getUTCMonth()+1}/${newDate.getUTCDate()}`, {
             'method': 'GET',
         });
+
+        if(!res2.ok) {
+            setTextLabel(`Failed retreiving logs, reload the tab and try again.`);
+            throw new Error('Error retrieving chat');
+        }
 
         data = await res2.text();
         const lines2 = eol.split(data);
         lines2.pop();
 
         lines = lines.concat(lines2);
-        newDate = moment(newDate).add(1, 'd').toDate();
+        newDate = moment(newDate).add(1, 'd').utc().startOf('d').toDate();
     }
  
     let toRet = [];
+    setTextLabel('Done.');
+
+    let colors: any = {};
+    colors.names = {
+        aqua: "#00ffff",
+        azure: "#f0ffff",
+        blue: "#0000ff",
+        brown: "#a52a2a",
+        cyan: "#00ffff",
+        fuchsia: "#ff00ff",
+        gold: "#ffd700",
+        green: "#008000",
+        indigo: "#4b0082",
+        khaki: "#f0e68c",
+        lightblue: "#add8e6",
+        lightcyan: "#e0ffff",
+        lightgreen: "#90ee90",
+        lightpink: "#ffb6c1",
+        lightyellow: "#ffffe0",
+        lime: "#00ff00",
+        magenta: "#ff00ff",
+        olive: "#808000",
+        orange: "#ffa500",
+        pink: "#ffc0cb",
+        purple: "#800080",
+        violet: "#800080",
+        red: "#ff0000",
+        yellow: "#ffff00"
+    };
+
+    colors.random = function() {
+        let result;
+        let count = 0;
+        for (let prop in this.names)
+            if (Math.random() < 1/++count)
+               result = prop;
+        return result;
+    };
+
+    const colorMap = new Map();
 
     for(let i = 0; i<lines.length; ++i) {
         const c = (lines[i].match(/^[^:]*(?::[^:]*){2}/g)[0].match(/([^ \[\]#]+)/g));
@@ -87,7 +139,8 @@ export const getChatLogs = async (creation: string, duration: number, username: 
                     'inSecs': inSecs,
                     'intoVod': strDifference(inSecs),
                     'user': c[3],
-                    'message': e
+                    'message': e,
+                    'userColor': colorMap.get(c[3]) ? colorMap.get(c[3]) : colorMap.set(c[3], colors.random()).get(c[3])
                 });
             }
         }
@@ -96,29 +149,27 @@ export const getChatLogs = async (creation: string, duration: number, username: 
     return toRet;
 }
 
-export const returnSearchedArray = (origArr: Array<Object|any>, pattern: string, isSensitive: boolean, isName: boolean): Array<Object> => {
+export const returnSearchedArray = (origArr: Array<Object|any>, pattern: string, isSensitive: boolean, isName: boolean, isRegex: boolean): Array<Object> => {
     let indexArray = []
     for(let i = 0; i < origArr.length; ++i) {
-        if(isName)  {
-            if(isSensitive) {
-                if(origArr[i].user === pattern) {
-                    indexArray.push(origArr[i]);
-                }
+        if(isRegex) {
+            if(origArr[i].message.match(pattern)) {
+                indexArray.push(origArr[i]);
             }
-            else {
-                if(origArr[i].user.toLowerCase() === pattern.toLowerCase()) {
-                    indexArray.push(origArr[i]);
-                }
+        }
+        else if(isName)  {
+            if(origArr[i].user.toLowerCase() === pattern.toLowerCase()) {
+                indexArray.push(origArr[i]);
             }
         }
         else {
             if(isSensitive) {
-                if(origArr[i].message.match(pattern)) {
+                if(origArr[i].message.includes(pattern)) {
                     indexArray.push(origArr[i]);
                 }
             }
             else {
-                if(origArr[i].message.toLowerCase().match(pattern.toLowerCase())) {
+                if(origArr[i].message.toLowerCase().includes(pattern.toLowerCase())) {
                     indexArray.push(origArr[i]);
                 }
             }
